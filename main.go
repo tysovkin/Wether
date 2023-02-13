093ca9396538236349f02f3043e57968211a3f64
tysovkin <122376832+tysovkin@users.noreply.github.com>
2023-02-14 02:27:39 +0300

    Update main.go

--- main.go
+++ main.go
@@ -38,7 +38,8 @@ func main() {
 			continue
 		}
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID,
-			fmt.Sprintf("Температура  %s  %.1f°С;  Облачность %.f%%;   Влажность %.1f%%;    Дождь за час  %.fmm  \n", city, weather.Temperature, weather.Clouds, weather.Humidity, weather.Rain, ))
+			fmt.Sprintf("Температура  %s  %.1f°С;  Облачность %.f%%;   Влажность %.1f%%;    Дождь за час  %.fmm  \n",
+				    city, weather.Temperature, weather.Clouds, weather.Humidity, weather.Rain, ))
 		bot.Send(msg)
 	}
 }
